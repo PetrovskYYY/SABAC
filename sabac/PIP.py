342b1f5c32ce6d27fd50c1cf26bfa85139768a43
Yuriy Petrovskiy <yuriy.petrovskiy@gmail.com>
2021-03-29 18:14:14 +0300

    2021-03-29
    Signed-off-by: Yuriy Petrovskiy <yuriy.petrovskiy@gmail.com>

--- sabac/PIP.py
+++ sabac/PIP.py
@@ -24,6 +24,8 @@ class InformationProvider:
     """
     Base class for information providers
     """
+    def __init__(self):
+        self.provided_attributes = None
 
     @classmethod
     def fetch(cls, request):
@@ -126,7 +128,7 @@ class PIP:
                 )
                 return False
             if isinstance(attribute_value['@contains'], list):
-                # If we have a list of values we check each of them
+                # If we have a list of values as an argument we check each of them
                 for item in attribute_value['@contains']:
                     if item in context_attribute_value:
                         return True
@@ -168,6 +170,13 @@ class PIP:
             self._providers_by_provided_attribute.setdefault(provided_attribute, []).append(provider)
 
     def fetch_attribute(self, attribute_name, request):
+        """
+        Fetches attribute value by given attribute name
+        :param attribute_name: Name of an attribute
+        :param request: SABAC request object - used for querying sub attributes
+        :return: found attribute value of None if attribute not available
+        (or error occurred during attribute value resolution)
+        """
         # logging.debug(f"Fetching attribute '{attribute_name}'...")
         # Avoiding search for known attributes
         if attribute_name in request.attributes:
@@ -175,9 +184,15 @@ class PIP:
 
         # Attribute is absent in context
         if attribute_name not in self._providers_by_provided_attribute:
-            # There is no way to get this attribute
-            logging.warning("No information providers found for attribute '%s'.", attribute_name)
-            return None
+            # There is no direct match for this attribute - will try to resolve
+            attribute_name_parts = attribute_name.split('.')
+            if len(attribute_name_parts) > 1:
+                # Attribute is complex - trying to resolve
+                return get_object_by_path(request.attributes, attribute_name_parts)
+            else:
+                # There is no way to get this attribute
+                logging.warning("No information providers found for attribute '%s'.", attribute_name)
+                return None
 
         for provider in self._providers_by_provided_attribute[attribute_name]:
             for required_attribute in provider.required_attributes:
@@ -190,4 +205,46 @@ class PIP:
             result = provider.fetch(request)
             if result is not None:
                 return result
+
+
+def get_object_by_path(root_object, path_parts, prefix=None):
+    """
+    Returns object using provided path and root object.
+    Functions treats dict keys and class properties as path parts
+    :param root_object: dict or class
+    :param path_parts: list of strings
+    :param prefix: prefix for path (used internally for recursion)
+    :return: Value of object that if found by given path or None if path resolution failed.
+    """
+    sub_object = None
+    if isinstance(root_object, dict) and path_parts[0] in root_object:
+        # Object is dict
+        sub_object = root_object[path_parts[0]]
+    elif hasattr(root_object, path_parts[0]):
+        # Object is some class that has required attribute or property
+        sub_object = getattr(root_object, path_parts[0])
+    else:
+        full_attribute_name = path_parts[0]
+        if prefix is not None:
+            full_attribute_name = f"{prefix}.{path_parts[0]}"
+        logging.warning(f"No information providers found for attribute '{full_attribute_name}'.")
+        # logging.debug(root_object)
+        return None
+
+    if len(path_parts) == 1:
+        # It is a last portion of given path
+        return sub_object
+
+    # There is at least one more level in path
+    new_prefix = prefix
+    if new_prefix is None:
+        new_prefix = path_parts[0]
+    else:
+        new_prefix = f"{new_prefix}.{path_parts[0]}"
+
+    return get_object_by_path(
+        root_object=sub_object,
+        path_parts=path_parts[1:],
+        prefix=new_prefix
+    )
 # EOF
