a3ce8ea055af90aaf2e67bac4f74197514042729
Yuriy Petrovskiy <yuriy.petrovskiy@gmail.com>
2024-08-31 15:29:07 +0300

    2024-08-31 Refactoring of actions

--- sabac/action.py
+++ sabac/action.py
@@ -11,26 +11,39 @@ __maintainer__ = "Yuriy Petrovskiy"
 __email__ = "yuriy.petrovskiy@gmail.com"
 
 import logging
-from .constants import RESULT_PERMIT, RESULT_DENY, PERMIT_SHORTCUTS, DENY_SHORTCUTS
+from dataclasses import dataclass, field
+from typing import Any, Optional, Dict
+
+from .constants import RESULT_PERMIT, RESULT_DENY, PERMIT_SHORTCUTS, DENY_SHORTCUTS, RuleEvaluationResult
 
 
+@dataclass(init=False)
 class Action:
+    action: Optional[str] = None
+    fulfill_on: Optional[RuleEvaluationResult] = None
+    attributes: Optional[Dict[str, Any]] = None
+
     def __init__(self, json_data):
         if not isinstance(json_data, dict):  # pragma: no cover
             raise ValueError("Dict should be provided by json_data attribute.")
 
+        self.extract_action_from_json(json_data)
+        self.extract_condition_from_json(json_data)
+        self.extract_attributes_from_json(json_data)
+
+    def extract_action_from_json(self, json_data):
         if 'action' in json_data:
             self.action = json_data['action']
         else:  # pragma: no cover
             raise ValueError("'action' attribute should be defined. %s" % json_data)
 
-        condition = None
+    def extract_condition_from_json(self, json_data):
         if 'fulfill_on' in json_data:
             condition = json_data['fulfill_on']
         elif 'if' in json_data:
             condition = json_data['if']
         else:  # pragma: no cover
-            raise ValueError("fulfill_on attribute should be defined.")
+            raise ValueError("fulfill_on attribute should be defined for any obligation or advice.")
 
         if condition:
             if condition in PERMIT_SHORTCUTS:
@@ -41,6 +54,7 @@ class Action:
                 logging.warning("Action element fulfill_on initialized with incorrect value: '%s'.", condition)
                 self.fulfill_on = condition
 
+    def extract_attributes_from_json(self, json_data):
         if 'attributes' in json_data:
             self.attributes = json_data['attributes']
         else:  # pragma: no cover
@@ -61,10 +75,10 @@ class Advice(Action):
     pass
 
 
+@dataclass()
 class ActionInstance:
-    def __init__(self):
-        self.source = None
-        self.attributes = {}
+    source: Any = None
+    attributes: Dict = field(default_factory={})
 
     def execute(self):
         raise NotImplementedError()  # pragma: no cover
