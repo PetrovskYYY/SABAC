55e2a4e6e5344c2a6b3b316068d85273d634d3b6
Yuriy Petrovskiy <yuriy.petrovskiy@gmail.com>
2026-04-29 20:32:49 +0300

    Typed `Request` class attributes and methods to improve type safety and added detailed type annotations.

--- sabac/request.py
+++ sabac/request.py
@@ -8,22 +8,27 @@ __copyright__ = "Copyright 2020, SABAC"
 __license__ = "LGPL"
 __email__ = "yuriy.petrovskiy@gmail.com"
 
+from typing import Any, Dict
+
 
 class Request:
-    def __init__(self, attributes, return_policy_id_list=False):
+    attributes: Dict[str, Any]
+    return_policy_id_list: bool
+
+    def __init__(self, attributes: Dict[str, Any], return_policy_id_list: bool = False) -> None:
         if attributes and isinstance(attributes, dict) and len(attributes) > 0:
             self.attributes = attributes
         else:  # pragma: no cover
             raise ValueError("Request should contain attributes: %s given." % attributes)
         self.return_policy_id_list = return_policy_id_list
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         result = "<Request data:"
         for key, value in self.attributes.items():
             result += "\n  %s: %s" % (key, value)
         result += "\n>"
         return result
 
-    def to_json(self):
+    def to_json(self) -> Dict[str, Any]:
         return self.attributes
 # EOF
